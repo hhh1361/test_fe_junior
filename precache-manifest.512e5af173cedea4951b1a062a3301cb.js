2622c98fcb9dcdd1dd67164bab1fd36b9bae01df
hhh1361 <nikita.turlyko@gmail.com>
2019-11-24 16:59:21 +0300

    Updates

---
+++ precache-manifest.512e5af173cedea4951b1a062a3301cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bda48654d2a52477ce7f0d3bcabbdc",
+    "url": "/test_fe_junior/index.html"
+  },
+  {
+    "revision": "743cf7fcc457420fb742",
+    "url": "/test_fe_junior/static/css/main.8e05381d.chunk.css"
+  },
+  {
+    "revision": "6ddcd2b91f700be8ae45",
+    "url": "/test_fe_junior/static/js/2.1b9bb9a2.chunk.js"
+  },
+  {
+    "revision": "743cf7fcc457420fb742",
+    "url": "/test_fe_junior/static/js/main.b3241693.chunk.js"
+  },
+  {
+    "revision": "d6914863543958a38a5b",
+    "url": "/test_fe_junior/static/js/runtime-main.05d960c7.js"
+  },
+  {
+    "revision": "cc9cec785b9861cd51bdd508d0b6899c",
+    "url": "/test_fe_junior/static/media/Illustration@1X.cc9cec78.png"
+  },
+  {
+    "revision": "2c63e4fef568c60490f686ce97abd1eb",
+    "url": "/test_fe_junior/static/media/lorem1.2c63e4fe.png"
+  }
+]);
